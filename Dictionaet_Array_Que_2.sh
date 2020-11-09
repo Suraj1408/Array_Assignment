658b95c07cafd950deab34eba05601d1b3241a8d
Suraj Gaikwad <surajgaikwad892@gmail.com.com>
2020-11-09 15:27:13 +0530

    Dictionary Array

---
+++ Dictionaet_Array_Que_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+declare -A birthRecord
+count=1
+
+while [ $count -lt 50 ]
+do
+        random=$(((( RANDOM % 12 )) + 1 ))
+        birthRecord[$random]=$(("${birthRecord[$random]}" + 1 ))
+        count=$(($count+1))
+done
+
+for(( i=1; i<=12; i++ ))
+do
+        echo "month $i ${birthRecord[$i]}"
+done
